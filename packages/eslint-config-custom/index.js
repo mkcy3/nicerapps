95e3189a0e3302492f03e1ac6ec9afd25b193317
milank <milankornicer@gmail.com>
2023-04-26 12:19:44 -0400

    feat: eslint adn ts configuration

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,11 +1,50 @@
 module.exports = {
-  extends: ['next', 'turbo', 'prettier'],
+  extends: [
+    'next',
+    'turbo',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:typescript-sort-keys/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+    'simple-import-sort',
+    'typescript-sort-keys',
+  ],
   rules: {
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': 'error',
+    'no-console': 1,
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+      },
+    ],
     '@next/next/no-html-link-for-pages': 'off',
   },
-  parserOptions: {
-    babelOptions: {
-      presets: [require.resolve('next/babel')],
+  settings: {
+    next: {
+      rootDir: ['apps/*/', 'packages/*/'],
+    },
+    react: {
+      version: 'detect',
     },
   },
+  ignorePatterns: [
+    '**/*.js',
+    '**/*.json',
+    'node_modules',
+    'public',
+    'styles',
+    '.next',
+    'coverage',
+    'dist',
+    '.turbo',
+  ],
 }
