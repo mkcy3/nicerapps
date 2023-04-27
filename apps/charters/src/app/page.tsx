98665dbd1c0bd8c6507b52b3e71ff4af242a7ddb
milank <milankornicer@gmail.com>
2023-04-26 20:28:47 -0400

    feat: playwright and github actions ci

--- apps/charters/src/app/page.tsx
+++ apps/charters/src/app/page.tsx
@@ -1,4 +1,4 @@
 const Page = () => {
-  return <h1 className="text-xl text-blue-500">Hello, Next.js!</h1>
+  return <h1 className="text-xl text-blue-500">Hello, A Next.js!</h1>
 }
 export default Page
---
+++ packages/e2e-tests/charters-tests/home.test.ts
@@ -0,0 +1,9 @@
+import { expect, test } from '@playwright/test'
+
+test('homepage has next.js on it', async ({ page }) => {
+  await page.goto('http://localhost:3000')
+
+  await expect(
+    page.getByRole('heading', { name: 'Hello, A Next.js!' })
+  ).toBeVisible()
+})
