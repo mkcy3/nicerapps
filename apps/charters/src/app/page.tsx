c7f941c2b84a3074ed57607f1b55c0fca1af4692
milank <milankornicer@gmail.com>
2023-05-15 15:00:54 -0400

    updating playwright fixing tests

--- apps/charters/src/app/page.tsx
+++ apps/charters/src/app/page.tsx
@@ -7,7 +7,7 @@ import Container from '@/components/ui/container'
 export default function HomePage() {
   return (
     <>
-      <Image
+      <img
         src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2830&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
         alt=""
         className="absolute inset-0 -z-10 h-full w-full object-cover"
