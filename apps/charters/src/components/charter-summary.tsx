8f34707b1812c86e4eeead5a7727ff137d737209
milank <milankornicer@gmail.com>
2023-05-15 15:11:25 -0400

    updated summary

--- apps/charters/src/components/charter-summary.tsx
+++ apps/charters/src/components/charter-summary.tsx
@@ -17,26 +17,45 @@ function CharterSummary({
 }: CharterSummaryProps) {
   const startFormat = start?.toDateString() ?? ''
 
-  const duration = start && end ? formatDistance(end, start) : null
+  const duration =
+    start && end ? `${formatDistance(end, start).charAt(0)} nights` : 'Day Sail'
 
   return (
     <section
       aria-labelledby="summary-heading"
-      className="bg-indigo-900 py-12 text-indigo-300 md:px-10 lg:col-start-2 lg:row-start-1 lg:mx-auto lg:w-full lg:max-w-lg lg:bg-transparent lg:px-0 lg:pb-24 lg:pt-0"
+      className="px-6 text-indigo-300 lg:col-start-2 lg:row-start-1 lg:mx-auto lg:w-full lg:max-w-lg"
     >
-      <div className="mx-auto max-w-2xl px-4 lg:max-w-none lg:px-0">
+      <div className=" mx-auto h-full max-w-2xl rounded-lg bg-indigo-900 px-6 lg:max-w-none">
         <h2 id="summary-heading" className="sr-only">
-          Order summary
+          Booking summary
         </h2>
 
-        <dl>
-          <dt className="text-sm font-medium">Amount due</dt>
-          <dd className="mt-1 text-3xl font-bold tracking-tight text-white">
-            $232.00
-          </dd>
-        </dl>
+        <dl></dl>
+
+        <ul
+          role="list"
+          className="divide-y divide-white divide-opacity-10 text-sm font-medium"
+        >
+          <li className="flex items-start space-x-4 py-6">
+            {/* <img
+                    src={product.imageSrc}
+                    alt={product.imageAlt}
+                    className="h-20 w-20 flex-none rounded-md object-cover object-center"
+                  /> */}
+            <div className="flex-auto space-y-1">
+              <h3 className="text-white">{startFormat}</h3>
+              <p>{duration}</p>
+            </div>
+          </li>
+        </ul>
 
         <dl className="space-y-6 border-t border-white border-opacity-10 pt-6 text-sm font-medium">
+          <div className="flex items-center justify-between">
+            <dt className="text-sm font-medium">Amount due</dt>
+            <dd className="mt-1 text-3xl font-bold tracking-tight text-white">
+              Stripe Price
+            </dd>
+          </div>
           <div className="flex items-center justify-between">
             <dt>Subtotal</dt>
             <dd>$570.00</dd>
