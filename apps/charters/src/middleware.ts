2e0aabe2fae366e59e141bacc1d7efc25292d5e9
milank <milankornicer@gmail.com>
2023-05-15 19:54:31 -0400

    upgrading clerk and middleware refactor

--- apps/charters/src/middleware.ts
+++ apps/charters/src/middleware.ts
@@ -1,33 +1,22 @@
-import { getAuth, withClerkMiddleware } from '@clerk/nextjs/server'
-import type { NextRequest } from 'next/server'
+import { authMiddleware } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
 
-// Set the paths that don't require the user to be signed in
-const publicPaths = ['/', '/login*', '/sign-up*', '/trip']
-
-const isPublic = (path: string) => {
-  return publicPaths.find((x) =>
-    path.match(new RegExp(`^${x}$`.replace('*$', '($|/)')))
-  )
-}
-
-export default withClerkMiddleware((request: NextRequest) => {
-  if (isPublic(request.nextUrl.pathname)) {
-    return NextResponse.next()
-  }
-  // if the user is not signed in redirect them to the sign in page.
-  const { userId } = getAuth(request)
-
-  if (!userId) {
-    // redirect the users to /pages/login/[[...index]].ts
-
-    const signInUrl = new URL('/login', request.url)
-    signInUrl.searchParams.set('redirect_url', request.url)
-    return NextResponse.redirect(signInUrl)
-  }
-  return NextResponse.next()
+export default authMiddleware({
+  publicRoutes: ['/', '/login*', '/sign-up*', '/trip'],
+  afterAuth(auth, req, evt) {
+    // handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      const signInUrl = new URL('/sign-in', req.url)
+      signInUrl.searchParams.set('redirect_url', req.url)
+      return NextResponse.redirect(signInUrl)
+    }
+    // rededirect them to organization selection page
+    if (!auth.orgId) {
+      const orgSelection = new URL('/org-selection', req.url)
+      return NextResponse.redirect(orgSelection)
+    }
+  },
 })
-
 // Stop Middleware running on static files and public folder
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
