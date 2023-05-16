c93e3dffdf732d8adddcd6be1aeb7bd96bf314d7
milank <milankornicer@gmail.com>
2023-05-16 16:43:24 -0400

    eslint error

--- apps/charters/src/app/book/page.tsx
+++ apps/charters/src/app/book/page.tsx
@@ -3,7 +3,7 @@ import Stripe from 'stripe'
 
 import { format } from '@/lib/day-of-year'
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: '2022-11-15',
 })
 
