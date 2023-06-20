3afd17c0055f34d66b253d4db5368103b625c481
milank <milankornicer@gmail.com>
2023-06-20 08:48:49 -0400

    feat: adding loading state to user-dashboard

--- apps/charters/src/app/(user)/account/page.tsx
+++ apps/charters/src/app/(user)/account/page.tsx
@@ -1,6 +1,5 @@
 import { Cancel, CheckCircle } from 'iconoir-react'
 
-import Container from '@/components/ui/container'
 import { getAuth } from '@/lib/clerk/server'
 import { getUserSucceededIntents } from '@/lib/stripe'
 import { formatAmountForDisplay } from '@/lib/utils'
@@ -10,115 +9,50 @@ export default async function AccountPage() {
   const intents = await getUserSucceededIntents(userId as string)
 
   return (
-    <Container className="min-h-screen pt-16">
-      <div className=" px-4 sm:px-6 lg:px-8">
-        <div className="sm:flex sm:items-center">
-          <div className="sm:flex-auto">
-            <h1 className="text-base font-semibold leading-6 text-gray-900">
-              Your Charters
-            </h1>
-          </div>
-        </div>
-        <div className="mt-8 flow-root ">
-          <div className="-mx-4 -my-2 overflow-x-auto  sm:-mx-6 lg:-mx-8">
-            <div className="inline-block min-w-full bg-gray-50 py-2 align-middle sm:px-6 lg:px-8">
-              <table className="min-w-full divide-y divide-gray-300">
-                <thead>
-                  <tr>
-                    <th
-                      scope="col"
-                      className="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
-                    >
-                      Transaction ID
-                    </th>
-                    <th
-                      scope="col"
-                      className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
-                    >
-                      Charter Starting
-                    </th>
-                    <th
-                      scope="col"
-                      className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
-                    >
-                      Passengers
-                    </th>
-                    <th
-                      scope="col"
-                      className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
-                    >
-                      Sleep Aboard
-                    </th>
-                    <th
-                      scope="col"
-                      className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
-                    >
-                      Payed
-                    </th>
-                    <th
-                      scope="col"
-                      className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
-                    >
-                      Remaining
-                    </th>
-                    <th
-                      scope="col"
-                      className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
-                    >
-                      Due Date
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="divide-y divide-gray-200 bg-white">
-                  {intents.length !== 0 ? (
-                    intents.map((intent) => (
-                      <tr key={intent.id}>
-                        <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0">
-                          {intent.id}
-                        </td>
-                        <td className="whitespace-nowrap px-2 py-2 text-sm font-medium text-gray-900">
-                          {intent.metadata.charterStartDate}
-                        </td>
-                        <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-900">
-                          {intent.metadata.passengers}
-                        </td>
-                        <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
-                          {intent.metadata.sleepAboard === 'true' ? (
-                            <CheckCircle className="text-green-500" />
-                          ) : (
-                            <Cancel className="text-red-500" />
-                          )}
-                        </td>
-                        <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
-                          {formatAmountForDisplay(intent.amount, 'CAD')}
-                        </td>
-                        <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
-                          {'N/A'}
-                        </td>
-                        <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
-                          {'N/A'}
-                        </td>
-                      </tr>
-                    ))
-                  ) : (
-                    <tr>
-                      <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0">
-                        No charters found
-                      </td>
-                      <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0"></td>
-                      <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0"></td>
-                      <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0"></td>
-                      <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0"></td>
-                      <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0"></td>
-                      <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0"></td>
-                    </tr>
-                  )}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
-    </Container>
+    <>
+      {intents.length !== 0 ? (
+        intents.map((intent) => (
+          <tr key={intent.id}>
+            <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0">
+              {intent.id}
+            </td>
+            <td className="whitespace-nowrap px-2 py-2 text-sm font-medium text-gray-900">
+              {intent.metadata.charterStartDate}
+            </td>
+            <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-900">
+              {intent.metadata.passengers}
+            </td>
+            <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
+              {intent.metadata.sleepAboard === 'true' ? (
+                <CheckCircle className="text-green-500" />
+              ) : (
+                <Cancel className="text-red-500" />
+              )}
+            </td>
+            <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
+              {formatAmountForDisplay(intent.amount, 'CAD')}
+            </td>
+            <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
+              {'N/A'}
+            </td>
+            <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
+              {'N/A'}
+            </td>
+          </tr>
+        ))
+      ) : (
+        <tr>
+          <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0">
+            No charters found
+          </td>
+          <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0"></td>
+          <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0"></td>
+          <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0"></td>
+          <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0"></td>
+          <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0"></td>
+          <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0"></td>
+        </tr>
+      )}
+    </>
   )
 }
